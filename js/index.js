756eec5b67effbb3948df83940c362e2efa45871
Hudson S. Carlos <hudsonscarlos@outlook.com>
2023-05-18 07:09:26 -0300

    feat: Aplicando aprendizado sobre elemento Flexbox CSS.

--- js/index.js
+++ js/index.js
@@ -16,6 +16,7 @@ function CaregarAreaJogoPPT(){
     divAreaJogo = document.getElementById(AREA_JOGO)
     divAreaJogo.style.backgroundColor = 'white';
     divAreaJogo.style.margin = '1% 1% 1% 1%';
+    divAreaJogo.style.display = 'flex';
     
     CaregarJogador1();
     CaregarJogador2();
@@ -23,27 +24,30 @@ function CaregarAreaJogoPPT(){
     divAreaJogo.appendChild(divJogador1);
     divAreaJogo.appendChild(divJogador2);
 }
-
+ 
 function CaregarJogador1(){
     // JOGADOR 1
     divJogador1 = document.createElement('div');
     divJogador1.setAttribute('id', 'divJogador1');
+    divJogador1.setAttribute('class', 'flex-item');
+    divJogador1.style.order = 0;
     divJogador1.style.width = '40%';
-    divJogador1.style.height = '200px';
-    divJogador1.style.margin = '10% 0% 10% 10%';
+    //divJogador1.style.height = '200px';
+    divJogador1.style.margin = '5%';
     divJogador1.style.backgroundColor = 'purple'    
-    divJogador1.style.display = 'inline-block';
+    
 }
 
 function CaregarJogador2(){
     // JOGADOR 2
     divJogador2 = document.createElement('div');
     divJogador2.setAttribute('id', 'divJogador2');
+    divJogador2.setAttribute('class', 'flex-item');
+    divJogador1.style.order = 1;
     divJogador2.style.width = '40%';
     divJogador2.style.height = '200px';
-    divJogador2.style.margin = '10% 10% 10% 0%';
+    divJogador2.style.margin = '5%';
     divJogador2.style.backgroundColor = 'green'
-    divJogador2.style.display = 'inline-block';
 
     let butPedra = document.createElement('button');
     let butPapel = document.createElement('button');
